04dc72b85a647a9b8cda98774216aadc402dd93b
NIJMA SAYYAD <nksayyad2900@outlook.com>
2020-12-30 09:21:28 +0530

    [NIJAM] Add:Welcome To AddressBook_System using ADO.NET

---
+++ AddressBookUsing_ADO.NET/AddressBookModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookUsing_ADO.NET
+{
+    class AddressBookModel
+    {
+    }
+}
---
+++ AddressBookUsing_ADO.NET/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AddressBookUsing_ADO.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***AddressBook_Using_ADO.NET***");
+            
+        }
+    }
+}
